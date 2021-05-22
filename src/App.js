5cc71f7b036125232d594b06e5802bf004cac4a5
Sherlock-B221 <mohanu526@gmail.com>
2021-05-23 03:09:16 +0530

    used useEffect without dependencies

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+    // const userLoggedInInfo =  localStorage.getItem('isLoggedIn');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // if (userLoggedInInfo === 'true')
+    //     setIsLoggedIn(true);    This will result in infinite loop bcz
+    //                             everytime it re-renders , it will re-render
+    //                             again on this if check
+    useEffect(() => {
+        const userLoggedInInfo =  localStorage.getItem('isLoggedIn');
+        if (userLoggedInInfo === 'true')
+            setIsLoggedIn(true);
+    },[]);
+    // useEffect runs after the function execution only if dependencies change
+
+
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', 'true');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <React.Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler}/>
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </React.Fragment>
+    );
+}
+
+export default App;
